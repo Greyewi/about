6eee88f208716dd6908f265be5cb894c95915e1f
Greyewi <greyewi@gmail.com>
2022-04-07 23:51:05 +0500

    add story frame

--- src/Pages/Portfolio/index.tsx
+++ src/Pages/Portfolio/index.tsx
@@ -2,6 +2,12 @@ import React, {useState} from 'react'
 import {Line, MapContainer, Point, Expand, Static, Addition} from './styles'
 import SchoolIcon from '@mui/icons-material/School'
 import BoyIcon from '@mui/icons-material/Boy';
+import WorkIcon from '@mui/icons-material/Work';
+import WorkspacePremiumIcon from '@mui/icons-material/WorkspacePremium';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import HomeWorkIcon from '@mui/icons-material/HomeWork';
+import PsychologyIcon from '@mui/icons-material/Psychology';
 
 const toggle = <T, >(arr: T[], item: T): T[] => {
     if (arr.includes(item)) return arr.filter(i => i !== item)
@@ -31,39 +37,45 @@ const historyMap: PointI[] = [
     {
         id: "the third time",
         date: '2015',
-        plot: "I have created my first web application."
+        plot: "I have created my first web application.",
+        addition: <WorkspacePremiumIcon fontSize="large"/>
     },
     {
         id: "the forth time",
         date: '2016',
-        plot: "I gain my fist job as developer"
+        plot: "I gain my fist job as developer",
+        addition: <WorkIcon fontSize="large"/>
     },
     {
         id: "the five time",
         date: '2017',
-        plot: "I gain my second job as developer"
+        plot: "I gain my second job as developer",
+        addition: <WorkHistoryIcon fontSize="large"/>
     },
     {
         id: "the six time",
         date: '2018',
-        plot: "I gain my third job as developer"
+        plot: "I gain my third job as developer",
+        addition: <LocationCityIcon fontSize="large"/>
     },
     {
         id: "the seven time",
         date: '2020',
-        plot: "I gain my fourth job as developer"
+        plot: "I gain my fourth job as developer",
+        addition: <HomeWorkIcon fontSize="large"/>
     },
     {
         id: "the eight time",
         date: '2020',
-        plot: "I leave out my fourth job and stated to work as a mentor"
+        plot: "I leave out my fourth job and stated to work as a mentor",
+        addition: <PsychologyIcon fontSize="large"/>
     },
 ]
 
 const Portfolio = () => {
     const [expanded, setExpanded] = useState<string[]>([])
     const pointerToggle = (item: string) => () => {
-        setExpanded(prev => toggle(prev, item))
+        setExpanded(prev => toggle<string>(prev, item))
     }
 
     return (
