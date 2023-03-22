29ab639df4590d2a83945c9c20faae702a4bb253
Greyewi <greyewi@gmail.com>
2023-03-22 10:37:55 +0200

    add currently and text printing

---
+++ src/Hooks/usePrintText.tsx
@@ -0,0 +1,19 @@
+import {useEffect, useRef, useState} from "react"
+
+export default function usePrintText(speed: number, text: string) {
+  const [legend, setLegend] = useState('')
+  const interval = useRef<(NodeJS.Timer | null)>(null)
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      setLegend(prev => {
+        if(prev.length >= (text.length - 1)) {
+          clearInterval(Number(interval.current))
+        }
+        return prev + text[prev.length]
+      })
+    }, Math.random() * speed)
+  }, [])
+
+  return legend
+}
--- src/Pages/Main/index.tsx
+++ src/Pages/Main/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import {Wrapper} from '../../styles'
 import {Legend, Description} from './styles'
+import { PopupWidget } from "react-calendly";
+import usePrintText from '../../Hooks/usePrintText'
+
+const fullLegend = `Hi there! I'm Suntsev Sergey.
+                I'm glad to see you on my site. Make yourself at home!`
 
 function Main() {
+    const legend = usePrintText(200, fullLegend)
     return (
         <Wrapper>
             <Legend>
-                Hi there! I'm Suntsev Sergey.
-                I'm glad to see you on my site. Make yourself at home!
+              {legend}
             </Legend>
             <Description>
                 I specialize in quality web development.<br/>
                 I show my journey on Instagram and commit code to Github.
             </Description>
-
+          <PopupWidget
+            url="https://calendly.com/greyewi/60min"
+            rootElement={document.getElementById("modal") || document.createElement('div')}
+            text="Click here to schedule!"
+            textColor="#1C2C35"
+            color="#B0FBBC"
+          />
         </Wrapper>
     )
 }
