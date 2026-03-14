c49597f412937422311ca332376fd639debf5066
Sergey Suntsev <greyewi@gmail.com>
2026-03-14 15:55:48 +0500

    fix: move Modal document.querySelector into useEffect for SSR

--- src/Components/Modal/index.tsx
+++ src/Components/Modal/index.tsx
@@ -1,10 +1,16 @@
-import { createPortal } from 'react-dom';
-import React, {FC} from 'react'
-const modalRoot = document.querySelector('#modal')
+import { createPortal } from 'react-dom'
+import React, { FC, useState, useEffect } from 'react'
 
-type Props = {children: React.ReactElement}
-export const Modal: FC<Props> = ({children}) => {
-    return <section>
-        {modalRoot && createPortal(children, modalRoot)}
-    </section>
+type Props = { children: React.ReactElement }
+
+export const Modal: FC<Props> = ({ children }) => {
+    const [modalRoot, setModalRoot] = useState<Element | null>(null)
+
+    useEffect(() => {
+        setModalRoot(document.querySelector('#modal'))
+    }, [])
+
+    if (!modalRoot) return null
+
+    return <section>{createPortal(children, modalRoot)}</section>
 }
