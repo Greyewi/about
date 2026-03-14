af39dcd11800028aa3c4e56048992975db7e9e6b
Sergey Suntsev <greyewi@gmail.com>
2026-03-14 16:34:42 +0500

    fix: add usePrintText cleanup for Strict Mode, add ServerStyleSheet for SSR fonts, upgrade TypeScript to 5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,42 +1,65 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
-export default function Document() {
-  return (
-    <Html lang="ru">
-      <Head>
-        <link rel="icon" href="/iconGreyewi.png" />
-        <style>{`
-          @font-face {
-            font-family: 'ModeSeven';
-            src: url('/fonts/Modeseven.ttf') format('truetype');
-            font-style: normal;
-            font-display: auto;
-          }
-          @font-face {
-            font-family: 'Legend';
-            src: url('/fonts/SuperLegend.ttf') format('truetype');
-            font-style: normal;
-            font-display: auto;
-          }
-          @font-face {
-            font-family: 'PressStart';
-            src: url('/fonts/PressStart.ttf') format('truetype');
-            font-style: normal;
-            font-display: auto;
-          }
-          @font-face {
-            font-family: 'RobotoSlab';
-            src: url('/fonts/RobotoSlab.ttf') format('truetype');
-            font-style: normal;
-            font-display: auto;
-          }
-        `}</style>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-        <div id="modal" />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="ru">
+        <Head>
+          <link rel="icon" href="/iconGreyewi.png" />
+          <style>{`
+            @font-face {
+              font-family: 'ModeSeven';
+              src: url('/fonts/Modeseven.ttf') format('truetype');
+              font-style: normal;
+              font-display: auto;
+            }
+            @font-face {
+              font-family: 'Legend';
+              src: url('/fonts/SuperLegend.ttf') format('truetype');
+              font-style: normal;
+              font-display: auto;
+            }
+            @font-face {
+              font-family: 'PressStart';
+              src: url('/fonts/PressStart.ttf') format('truetype');
+              font-style: normal;
+              font-display: auto;
+            }
+            @font-face {
+              font-family: 'RobotoSlab';
+              src: url('/fonts/RobotoSlab.ttf') format('truetype');
+              font-style: normal;
+              font-display: auto;
+            }
+          `}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal" />
+        </body>
+      </Html>
+    )
+  }
 }
--- src/Hooks/usePrintText.tsx
+++ src/Hooks/usePrintText.tsx
@@ -2,17 +2,22 @@ import {useEffect, useRef, useState} from "react"
 
 export default function usePrintText(speed: number, text: string) {
   const [legend, setLegend] = useState('')
-  const interval = useRef<(NodeJS.Timer | null)>(null)
+  const interval = useRef<ReturnType<typeof setInterval> | null>(null)
 
   useEffect(() => {
     interval.current = setInterval(() => {
       setLegend(prev => {
         if(prev.length >= (text.length - 1)) {
-          clearInterval(Number(interval.current))
+          clearInterval(interval.current!)
         }
         return prev + text[prev.length]
       })
     }, speed)
+
+    return () => {
+      clearInterval(interval.current!)
+      interval.current = null
+    }
   }, [speed, text])
 
   return legend
